0f12219f552131ce234ec553cf4dd2a17ee4a756
Mustafa Saeed <mustafaksaeed@gmail.com>
2025-06-12 17:43:19 -0400

    added mock database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs";
 
 const port = 8000;
 const app = express();
@@ -18,23 +19,21 @@ app.post("/signup", (req, res) => {
   res.send("ai lied");
 });
 
-const Users = [
-  { id: 1, email: "test@example.com", password: "password123" },
-  { id: 2, email: "another@user.com", password: "securepassword" },
-];
 app.post("/login", (req, res) => {
-  console.log("request body", req.body);
   const { email: emailVal, password: passwordVal } = req.body;
 
-  const user = Users.find(
-    (info) => info.email === emailVal && info.password === passwordVal
-  );
+  const rawData = fs.readFileSync("./Database/users", "utf8");
+  let users = JSON.parse(rawData);
 
-  if (user === undefined) {
-    res.status(200).json({ message: "login failed" });
-  } else {
-    res.status(200).json({ message: "login success" });
-  }
+  const newUser = {
+    id: Date.now(),
+    email: emailVal,
+    password: passwordVal,
+  };
+
+  users.push(newUser);
+  fs.writeFileSync("./Database/users", JSON.stringify(users, null, 2), "utf8");
+  res.send({
+    msg: "CREATED USER",
+  });
 });
-
-console.log("users", Users);
