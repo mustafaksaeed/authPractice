652471eca687f35e341004d072484e189e699aa0
Mustafa Saeed <mustafaksaeed@gmail.com>
2025-06-14 00:55:13 -0400

    added home title to home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>
-    
-  </div>;
+  return <div>Home</div>;
 };
 
 export default App;
